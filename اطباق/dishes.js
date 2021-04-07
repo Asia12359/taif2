29da938eb28a116b41d587fc75439912f361639b
Asia12359 <81997795+Asia12359@users.noreply.github.com>
2021-04-07 20:32:39 +0300

    test

---
+++ اطباق/dishes.js
@@ -0,0 +1,138 @@
+/* jslint browser:true */
+/* global window */
+var up1 = document.getElementById("up1");
+var down1 = document.getElementById("down1");
+var  text1 = document.getElementById("slow1");
+up1.onclick = function () {
+    "use strict";
+    up1.style.display = "none";
+    down1.style.display = "block";
+    text1.style.display = "block";
+
+};
+down1.onclick = function () {
+    "use strict";
+    down1.style.display = "none";
+    text1.style.display = "none";
+    up1.style.display = "block";
+};
+
+var up2 = document.getElementById("up2");
+var down2 = document.getElementById("down2");
+var text2 = document.getElementById("slow2");
+up2.onclick = function () {
+    "use strict";
+    up2.style.display = "none";
+    down2.style.display = "block";
+    text2.style.display = "block";
+
+};
+down2.onclick = function () {
+    "use strict";
+    down2.style.display = "none";
+    text2.style.display = "none";
+    up2.style.display = "block";
+};
+
+var up3 = document.getElementById("up3");
+var down3 = document.getElementById("down3");
+var text3 = document.getElementById("slow3");
+up3.onclick = function () {
+    "use strict";
+    up3.style.display = "none";
+    down3.style.display = "block";
+    text3.style.display = "block";
+
+};
+down3.onclick = function () {
+    "use strict";
+    down3.style.display = "none";
+    text3.style.display = "none";
+    up3.style.display = "block";
+};
+
+var up4 = document.getElementById("up4");
+var down4 = document.getElementById("down4");
+var text4 = document.getElementById("slow4");
+up4.onclick = function () {
+    "use strict";
+    up4.style.display = "none";
+    down4.style.display = "block";
+    text4.style.display = "block";
+
+};
+down4.onclick = function () {
+    "use strict";
+    down4.style.display = "none";
+    text4.style.display = "none";
+    up4.style.display = "block";
+};
+
+var up5 = document.getElementById("up5");
+var down5 = document.getElementById("down5");
+var text5 = document.getElementById("slow5");
+up5.onclick = function () {
+    "use strict";
+    up5.style.display = "none";
+    down5.style.display = "block";
+    text5.style.display = "block";
+
+};
+down5.onclick = function () {
+    "use strict";
+    down5.style.display = "none";
+    text5.style.display = "none";
+    up5.style.display = "block";
+};
+
+var up6 = document.getElementById("up6");
+var down6 = document.getElementById("down6");
+var text6 = document.getElementById("slow6");
+up6.onclick = function () {
+    "use strict";
+    up6.style.display = "none";
+    down6.style.display = "block";
+    text6.style.display = "block";
+
+};
+down6.onclick = function () {
+    "use strict";
+    down6.style.display = "none";
+    text6.style.display = "none";
+    up6.style.display = "block";
+};
+
+var up7 = document.getElementById("up7");
+var down7 = document.getElementById("down7");
+var text7 = document.getElementById("slow7");
+up7.onclick = function () {
+    "use strict";
+    up7.style.display = "none";
+    down7.style.display = "block";
+    text7.style.display = "block";
+
+};
+down7.onclick = function () {
+    "use strict";
+    down7.style.display = "none";
+    text7.style.display = "none";
+    up7.style.display = "block";
+};
+
+var up8 = document.getElementById("up8");
+var down8 = document.getElementById("down8");
+var text8 = document.getElementById("slow8");
+up8.onclick = function () {
+    "use strict";
+    up8.style.display = "none";
+    down8.style.display = "block";
+    text8.style.display = "block";
+
+};
+down8.onclick = function () {
+    "use strict";
+    down8.style.display = "none";
+    text8.style.display = "none";
+    up8.style.display = "block";
+};
+
